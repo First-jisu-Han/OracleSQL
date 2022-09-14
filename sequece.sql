eafc6007ad1b361f285144fd0d17b5c0fb02f750
First-jisu-Han <weblite@naver.com>
2022-09-14 13:40:23 +0900

    sequence insert

--- sequece.sql
+++ sequece.sql
@@ -1,4 +1,4 @@
-CREATE SEQUNECE his_squ 
+CREATE SEQUNECE his_seq 
 START WITH 100 
 INCREMENT BY 1 
 MAXVALUE 500000 
@@ -6,3 +6,7 @@ MINVALUE 100
 CYCLE 
 ORDER 
 CACHE 20; 
+
+
+
+INSERT INTO BANKINGMASTER ( bank_id , bank_name , bank_dvcd , bank_dvcd_nm ) VALUES ( his_seq.NEXTVAL,' pbank' , '1213' , '중규모') 
